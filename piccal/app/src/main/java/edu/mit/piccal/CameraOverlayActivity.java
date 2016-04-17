de4f4045c8194d3d8550ee3f54e99bd4e88bb6b6
Jake Barnwell <jb16@mit.edu>
2016-04-16 23:09:49 -0400

    Preview now works (demo copied from github)

--- piccal/app/src/main/java/edu/mit/piccal/CameraOverlayActivity.java
+++ piccal/app/src/main/java/edu/mit/piccal/CameraOverlayActivity.java
@@ -1,125 +1,124 @@
 package edu.mit.piccal;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.hardware.Camera;
-import android.graphics.PixelFormat;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
+import android.widget.Toast;
 
-import java.io.IOException;
-
-public class CameraOverlayActivity extends AppCompatActivity {
-
-    private Camera mCamera = null;
-    private CameraView mCameraView = null;
-    private final static String LOG_HEADER = "piccal - CameraOverlay";
+public class CameraOverlayActivity extends Activity {
+    private SurfaceView preview=null;
+    private SurfaceHolder previewHolder=null;
+    private Camera camera=null;
+    private boolean inPreview=false;
+    private boolean cameraConfigured=false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d(LOG_HEADER, "Camera Overlay Activity onCreate.");
-
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.content_camera_overlay);
+
+        preview=(SurfaceView)findViewById(R.id.preview);
+        previewHolder=preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
-
-    public void onCameraButtonClick(View view) {
-        try{
-            mCamera = Camera.open();//you can use open(int) to use different cameras
-        } catch (Exception e){
-            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
-        }
-
-        Button camerabutton = (Button)findViewById(R.id.camera_button);
-        Log.d(LOG_HEADER, "camerabutton is null : " + (camerabutton == null));
-
-        if(mCamera != null) {
-            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
-            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-            Log.d(LOG_HEADER, "mCameraView is : " + mCameraView.toString());
-            if(camera_view == null) {
-                Log.d(LOG_HEADER, "camera_view is null");
-            } else {
-                Log.d(LOG_HEADER, "camera_view is null : " + camera_view.toString());
-            }
-            camera_view.addView(mCameraView);//add the SurfaceView to the layout
-            // the above line is where we get a NullPointerException
-        }
-
-        //btn to close the application
-        Button imgClose = (Button)findViewById(R.id.camera_button);
-        imgClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                System.exit(0);
-                ;
-            }
-        });
-    }
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.d(LOG_HEADER, "Camera Overlay Activity on create view.");
-        return null;
-    }
-
-    protected void onActivityCreated(Bundle savedInstanceState) {
-        Log.d(LOG_HEADER, "Camera Overlay Activity on activity created.");
-    }
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        ;
-//    }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_HEADER, "Camera Overlay Activity onStart.");
+    public void onResume() {
+        super.onResume();
 
-//        try{
-//            mCamera = Camera.open();//you can use open(int) to use different cameras
-//        } catch (Exception e){
-//            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
-//        }
-//
-//        Button camerabutton = (Button)findViewById(R.id.camera_button);
-//        Log.d(LOG_HEADER, "camerabutton is null : " + (camerabutton == null));
-//
-//
-//        if(mCamera != null) {
-//            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
-//            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-//            Log.d(LOG_HEADER, "mCameraView is : " + mCameraView.toString());
-//            if(camera_view == null) {
-//                Log.d(LOG_HEADER, "camera_view is null");
-//            } else {
-//                Log.d(LOG_HEADER, "camera_view is null : " + camera_view.toString());
-//            }
-//            camera_view.addView(mCameraView);//add the SurfaceView to the layout
-//            // the above line is where we get a NullPointerException
-//        }
-//
-//        //btn to close the application
-//        Button imgClose = (Button)findViewById(R.id.camera_button);
-//        imgClose.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                System.exit(0);
-//                ;
-//            }
-//        });
+        camera=Camera.open();
+        startPreview();
     }
 
+    @Override
+    public void onPause() {
+        if (inPreview) {
+            camera.stopPreview();
+        }
 
+        camera.release();
+        camera=null;
+        inPreview=false;
+
+        super.onPause();
+    }
+
+    private Camera.Size getBestPreviewSize(int width, int height,
+                                           Camera.Parameters parameters) {
+        Camera.Size result=null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width<=width && size.height<=height) {
+                if (result==null) {
+                    result=size;
+                }
+                else {
+                    int resultArea=result.width*result.height;
+                    int newArea=size.width*size.height;
+
+                    if (newArea>resultArea) {
+                        result=size;
+                    }
+                }
+            }
+        }
+
+        return(result);
+    }
+
+    private void initPreview(int width, int height) {
+        if (camera!=null && previewHolder.getSurface()!=null) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            }
+            catch (Throwable t) {
+                Log.e("PreviewDemo-surfaceCallback",
+                        "Exception in setPreviewDisplay()", t);
+//                Toast
+//                        .makeText(PreviewDemo.this, t.getMessage(), Toast.LENGTH_LONG)
+//                        .show();
+            }
+
+            if (!cameraConfigured) {
+                Camera.Parameters parameters=camera.getParameters();
+                Camera.Size size=getBestPreviewSize(width, height,
+                        parameters);
+
+                if (size!=null) {
+                    parameters.setPreviewSize(size.width, size.height);
+                    camera.setParameters(parameters);
+                    cameraConfigured=true;
+                }
+            }
+        }
+    }
+
+    private void startPreview() {
+        if (cameraConfigured && camera!=null) {
+            camera.startPreview();
+            inPreview=true;
+        }
+    }
+
+    SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
+        public void surfaceCreated(SurfaceHolder holder) {
+            // no-op -- wait until surfaceChanged()
+        }
+
+        public void surfaceChanged(SurfaceHolder holder,
+                                   int format, int width,
+                                   int height) {
+            initPreview(width, height);
+            startPreview();
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // no-op
+        }
+    };
 }
