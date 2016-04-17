4f742d8fb84c6c11a214043ef74f33213372f44e
Jake Barnwell <jb16@mit.edu>
2016-04-16 22:38:19 -0400

    Still debugging

--- piccal/app/src/main/java/edu/mit/piccal/CameraOverlayActivity.java
+++ piccal/app/src/main/java/edu/mit/piccal/CameraOverlayActivity.java
@@ -36,16 +36,15 @@ public class CameraOverlayActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_HEADER, "Camera Overlay Activity onStart.");
-
+    public void onCameraButtonClick(View view) {
         try{
             mCamera = Camera.open();//you can use open(int) to use different cameras
         } catch (Exception e){
             Log.d("ERROR", "Failed to get camera: " + e.getMessage());
         }
+
+        Button camerabutton = (Button)findViewById(R.id.camera_button);
+        Log.d(LOG_HEADER, "camerabutton is null : " + (camerabutton == null));
 
         if(mCamera != null) {
             mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
@@ -69,6 +68,57 @@ public class CameraOverlayActivity extends AppCompatActivity {
                 ;
             }
         });
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(LOG_HEADER, "Camera Overlay Activity on create view.");
+        return null;
+    }
+
+    protected void onActivityCreated(Bundle savedInstanceState) {
+        Log.d(LOG_HEADER, "Camera Overlay Activity on activity created.");
+    }
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        ;
+//    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_HEADER, "Camera Overlay Activity onStart.");
+
+//        try{
+//            mCamera = Camera.open();//you can use open(int) to use different cameras
+//        } catch (Exception e){
+//            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+//        }
+//
+//        Button camerabutton = (Button)findViewById(R.id.camera_button);
+//        Log.d(LOG_HEADER, "camerabutton is null : " + (camerabutton == null));
+//
+//
+//        if(mCamera != null) {
+//            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+//            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+//            Log.d(LOG_HEADER, "mCameraView is : " + mCameraView.toString());
+//            if(camera_view == null) {
+//                Log.d(LOG_HEADER, "camera_view is null");
+//            } else {
+//                Log.d(LOG_HEADER, "camera_view is null : " + camera_view.toString());
+//            }
+//            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+//            // the above line is where we get a NullPointerException
+//        }
+//
+//        //btn to close the application
+//        Button imgClose = (Button)findViewById(R.id.camera_button);
+//        imgClose.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                System.exit(0);
+//                ;
+//            }
+//        });
     }
 
 
