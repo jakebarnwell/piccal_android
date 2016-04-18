8d19ec289c6919f55a1a0191665017cd247fdbf8
Jake Barnwell <jbarnwell@mit.edu>
2016-04-17 21:43:16 -0400

    Remove CameraView class (unused) and update camera preview example so that picture taking and rotation works

--- piccal/app/src/main/java/edu/mit/piccal/CameraOverlayActivity.java
+++ piccal/app/src/main/java/edu/mit/piccal/CameraOverlayActivity.java
@@ -1,14 +1,23 @@
 package edu.mit.piccal;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 // Taken from https://github.com/commonsguy/cw-advandroid/tree/master/Camera/Preview
+//  and https://github.com/commonsguy/cw-advandroid/tree/master/Camera/Picture
 
 public class CameraOverlayActivity extends Activity {
     private SurfaceView preview=null;
@@ -28,6 +37,46 @@ public class CameraOverlayActivity extends Activity {
         previewHolder=preview.getHolder();
         previewHolder.addCallback(surfaceCallback);
         previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void onClick_takePicture(View view) {
+        if (inPreview) {
+            camera.takePicture(null, null, photoCallback);
+            inPreview=false;
+        }
+    }
+
+    Camera.PictureCallback photoCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            new SavePhotoTask().execute(data);
+            camera.startPreview();
+            inPreview=true;
+        }
+    };
+
+    class SavePhotoTask extends AsyncTask<byte[], String, String> {
+        @Override
+        protected String doInBackground(byte[]... jpeg) {
+            File photo=
+                    new File(Environment.getExternalStorageDirectory(),
+                            "photo.jpg");
+
+            if (photo.exists()) {
+                photo.delete();
+            }
+
+            try {
+                FileOutputStream fos=new FileOutputStream(photo.getPath());
+
+                fos.write(jpeg[0]);
+                fos.close();
+            }
+            catch (java.io.IOException e) {
+                Log.e("PictureDemo", "Exception in photoCallback", e);
+            }
+
+            return(null);
+        }
     }
 
     @Override
@@ -103,12 +152,13 @@ public class CameraOverlayActivity extends Activity {
 
     private void startPreview() {
         if (cameraConfigured && camera!=null) {
+            setCameraOrientation();
             camera.startPreview();
             inPreview=true;
         }
     }
 
-    SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
+    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
         public void surfaceCreated(SurfaceHolder holder) {
             // no-op -- wait until surfaceChanged()
         }
@@ -116,6 +166,7 @@ public class CameraOverlayActivity extends Activity {
         public void surfaceChanged(SurfaceHolder holder,
                                    int format, int width,
                                    int height) {
+            setCameraOrientation();
             initPreview(width, height);
             startPreview();
         }
@@ -124,4 +175,18 @@ public class CameraOverlayActivity extends Activity {
             // no-op
         }
     };
+
+    private void setCameraOrientation() {
+        int rotation = this.getWindowManager().getDefaultDisplay().getRotation();
+        int to_rotate = 0;
+
+        switch(rotation) {
+            case Surface.ROTATION_0: to_rotate = 90; break;
+            case Surface.ROTATION_90: to_rotate = 0; break;
+            case Surface.ROTATION_180: to_rotate = 270; break;
+            case Surface.ROTATION_270: to_rotate = 180; break;
+        }
+
+        camera.setDisplayOrientation(to_rotate);
+    }
 }
