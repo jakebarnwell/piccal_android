5eb268bf86fd9449e1eba057b55e99a33af6f761
Jake Barnwell <jbarnwell@mit.edu>
2016-04-19 12:58:25 -0400

    OCR Now works on CameraOverlay activity

--- piccal/app/src/main/java/edu/mit/piccal/CameraOverlayActivity.java
+++ piccal/app/src/main/java/edu/mit/piccal/CameraOverlayActivity.java
@@ -1,6 +1,8 @@
 package edu.mit.piccal;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,8 +19,13 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 // Taken from https://github.com/commonsguy/cw-advandroid/tree/master/Camera/Preview
 //  and https://github.com/commonsguy/cw-advandroid/tree/master/Camera/Picture
@@ -28,12 +35,24 @@ public class CameraOverlayActivity extends Activity {
     private ImageView imview = null;
     private int imViewWidth, imViewHeight;
     private SurfaceHolder previewHolder = null;
-    private Camera camera=null;
-    private boolean inPreview=false;
-    private boolean cameraConfigured=false;
+    private Camera camera = null;
+    private boolean inPreview = false;
+    private boolean cameraConfigured = false;
     private final static String LOG_HEADER = "piccal - CameraOverlay";
 
     private enum State {CAMERA_PREVIEW, IMAGE_REVIEW};
+
+    // For OCR:
+    private TessBaseAPI baseApi = new TessBaseAPI();
+    public static final String PACKAGE_NAME = "edu.mit.piccal";
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/piccal/";
+
+    // You should have the trained data file in assets folder
+    // You can get them at:
+    // http://code.google.com/p/tesseract-ocr/downloads/list
+    public static final String LANG = "eng";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,31 +60,74 @@ public class CameraOverlayActivity extends Activity {
 
         setContentView(R.layout.content_camera_overlay);
 
-        preview=(SurfaceView)findViewById(R.id.preview);
-        previewHolder=preview.getHolder();
+        preview = (SurfaceView)findViewById(R.id.preview);
+        previewHolder = preview.getHolder();
         previewHolder.addCallback(surfaceCallback);
         previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
         imview = (ImageView)findViewById(R.id.imageView);
+
+        // Stuff for OCR:
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(LOG_HEADER, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(LOG_HEADER, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+
+        // lang.traineddata file with the app (in assets folder)
+        // You can get them at:
+        // http://code.google.com/p/tesseract-ocr/downloads/list
+        // This area needs work and optimization
+        if (!(new File(DATA_PATH + "tessdata/" + LANG + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+                InputStream in = assetManager.open("tessdata/" + LANG + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + LANG + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+
+                Log.v(LOG_HEADER, "Copied " + LANG + " traineddata");
+            } catch (IOException e) {
+                Log.e(LOG_HEADER, "Was unable to copy " + LANG + " traineddata " + e.toString());
+            }
+        }
+
+        baseApi.setDebug(true);
+        Log.d(LOG_HEADER, DATA_PATH);
+        baseApi.init(DATA_PATH, LANG);
     }
 
     public void onClick_takePicture(View view) {
         if(inPreview) {
             imViewWidth = preview.getWidth();
             imViewHeight = preview.getHeight();
-            camera.takePicture(null, null, photoCallback2);
+            camera.takePicture(null, null, photoCallback);
         }
 
     }
 
     Camera.PictureCallback photoCallback = new Camera.PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
-            new SavePhotoTask().execute(data);
-            startPreview();
-        }
-    };
-
-    Camera.PictureCallback photoCallback2 = new Camera.PictureCallback() {
         public void onPictureTaken(byte[] data, Camera camera) {
             // Start to save the photo
             new SavePhotoTask().execute(data);
@@ -84,6 +146,14 @@ public class CameraOverlayActivity extends Activity {
 
             if(bitmap_image != null) {
                 imview.setImageBitmap(bitmap_image);
+
+                // Do OCR:
+                baseApi.setImage(bitmap_image);
+                String recognizedText = baseApi.getUTF8Text();
+                Context context = getApplicationContext();
+                Toast.makeText(context, recognizedText, Toast.LENGTH_SHORT).show();
+                Log.d(LOG_HEADER, recognizedText);
+                //baseApi.end();
             }
         }
     };
@@ -96,7 +166,6 @@ public class CameraOverlayActivity extends Activity {
         // Get the dimensions of the bitmap
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
         bmOptions.inJustDecodeBounds = true;
-//            BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
         int photoW = bmOptions.outWidth;
         int photoH = bmOptions.outHeight;
 
@@ -112,6 +181,7 @@ public class CameraOverlayActivity extends Activity {
 
         Matrix mtx = new Matrix();
         mtx.postRotate(90);
+
         // Rotating Bitmap
         Bitmap rotatedBMP = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), mtx, true);
 
@@ -149,27 +219,20 @@ public class CameraOverlayActivity extends Activity {
     class SavePhotoTask extends AsyncTask<byte[], String, String> {
         @Override
         protected String doInBackground(byte[]... jpeg) {
-
-
-
-            /* ------------------------- */
-
-            File photo=
-                    new File(Environment.getExternalStorageDirectory(),
-                            "photo.jpg");
+            File photo = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
 
             if (photo.exists()) {
                 photo.delete();
             }
 
             try {
-                FileOutputStream fos=new FileOutputStream(photo.getPath());
+                FileOutputStream fos = new FileOutputStream(photo.getPath());
 
                 fos.write(jpeg[0]);
                 fos.close();
             }
             catch (java.io.IOException e) {
-                Log.e("PictureDemo", "Exception in photoCallback", e);
+                Log.e(LOG_HEADER, "Exception in SavePhotoTask: ", e);
             }
 
             return(null);
@@ -180,7 +243,7 @@ public class CameraOverlayActivity extends Activity {
     public void onResume() {
         super.onResume();
 
-        camera=Camera.open();
+        camera = Camera.open();
         startPreview();
     }
 
@@ -191,31 +254,21 @@ public class CameraOverlayActivity extends Activity {
         super.onPause();
     }
 
-    public void stopPreview() {
-        if(inPreview) {
-            camera.stopPreview();
-        }
-
-        camera.release();
-        camera = null;
-        inPreview = false;
-    }
-
     private Camera.Size getBestPreviewSize(int width, int height,
                                            Camera.Parameters parameters) {
-        Camera.Size result=null;
+        Camera.Size result = null;
 
         for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-            if (size.width<=width && size.height<=height) {
-                if (result==null) {
-                    result=size;
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
                 }
                 else {
-                    int resultArea=result.width*result.height;
-                    int newArea=size.width*size.height;
+                    int resultArea = result.width*result.height;
+                    int newArea = size.width*size.height;
 
-                    if (newArea>resultArea) {
-                        result=size;
+                    if (newArea > resultArea) {
+                        result = size;
                     }
                 }
             }
@@ -225,7 +278,7 @@ public class CameraOverlayActivity extends Activity {
     }
 
     private void initPreview(int width, int height) {
-        if (camera!=null && previewHolder.getSurface()!=null) {
+        if (camera != null && previewHolder.getSurface() != null) {
             try {
                 camera.setPreviewDisplay(previewHolder);
             }
@@ -238,25 +291,35 @@ public class CameraOverlayActivity extends Activity {
             }
 
             if (!cameraConfigured) {
-                Camera.Parameters parameters=camera.getParameters();
-                Camera.Size size=getBestPreviewSize(width, height,
+                Camera.Parameters parameters = camera.getParameters();
+                Camera.Size size = getBestPreviewSize(width, height,
                         parameters);
 
-                if (size!=null) {
+                if (size != null) {
                     parameters.setPreviewSize(size.width, size.height);
                     camera.setParameters(parameters);
-                    cameraConfigured=true;
+                    cameraConfigured = true;
                 }
             }
         }
     }
 
     private void startPreview() {
-        if (cameraConfigured && camera!=null) {
+        if (cameraConfigured && camera != null) {
             setCameraOrientation();
             camera.startPreview();
-            inPreview=true;
+            inPreview = true;
         }
+    }
+
+    public void stopPreview() {
+        if(inPreview) {
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera = null;
+        inPreview = false;
     }
 
     SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
